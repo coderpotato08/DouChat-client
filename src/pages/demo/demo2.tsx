f67c122f493472ee02d48e55cee6b981a73db7fb
haiyu.lu <haiyu.lu@ly.com>
2025-01-17 17:14:28 +0800

    feat: sse demo

--- src/pages/demo/demo2.tsx
+++ src/pages/demo/demo2.tsx
@@ -1,5 +1,20 @@
+import { useEffect } from "react";
+import { EventSourceMessage, fetchEventSource } from '@fortaine/fetch-event-source';
 export const Demo2 = () => {
 
+  useEffect(() => {
+    fetchEventSource('http://localhost:3030/ai/test-sse', {
+      onopen: async () => {
+        console.log('open')
+      },
+      onmessage: (e: EventSourceMessage) => {
+        console.log(e, Date.now())
+      },
+      onclose: () => {
+        console.log('close')
+      },
+    })
+  }, [])
 
   return <div style={{ position: "relative", width: "100%", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
   </div>
